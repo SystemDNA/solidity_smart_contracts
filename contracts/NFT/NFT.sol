cb9031ba611414b7f236b718cbfe9b80e4c93b31
Gnanananda Babu <babu.g@systemsdna.com>
2023-03-16 19:55:29 +0530

    update 6

--- contracts/NFT/NFT.sol
+++ contracts/NFT/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-//=============19.51 comment updated ==========
+//=============19.55 comment updated ==========
 // Amended by HashLips
 /**
     !Disclaimer!
@@ -11,6 +11,7 @@
     HashLips will not be liable in any way if for the use 
     of the code. That being said, the code has been tested 
     to the best of the developers' knowledge to work as intended.
+    //=============19.55 comment updated ==========
 */
 
 pragma solidity >=0.7.0 <0.9.0;
